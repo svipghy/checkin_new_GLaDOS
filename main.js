0e43863886606a1eac18709cfee50c723e78a872
高宏宇 <60728331+svipghy@users.noreply.github.com>
2023-07-27 22:21:38 +0800

    Update main.js

--- main.js
+++ main.js
@@ -18,14 +18,14 @@ const glados = async () => {
     }).then((r) => r.json())
     return [
       'Checkin OK ',
-      `${checkin.message}`,
       ` Left Days ${Number(status.data.leftDays)}`,
+      `${checkin.message}`,
     ]
   } catch (error) {
     return [
       'Checkin Error ',
-      `${error}`,
       `<${process.env.GITHUB_SERVER_URL}/${process.env.GITHUB_REPOSITORY}>`,
+      `${error}`,
     ]
   }
 }
